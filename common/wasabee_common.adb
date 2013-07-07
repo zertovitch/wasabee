88089a5914cfd75ca47ac20e22ec782ead2536e4
gdemont <gdemont@d0ae7f95-e485-46ef-b4d0-40183c02ebb0>
2013-07-07 16:41:03 +0000

    Initial files, with an 'empty shell' project for the GWindows (native Windows) target

---
+++ common/wasabee_common.adb
@@ -0,0 +1,14 @@
+package body Wasabee_common is
+
+   ----------------
+   -- Load_frame --
+   ----------------
+
+   procedure Load_frame (object: in out HTML_object; URL: String) is
+   begin
+      --  Generated stub: replace with real body!
+      pragma Compile_Time_Warning (Standard.True, "Load_frame unimplemented");
+      raise Program_Error;
+   end Load_frame;
+
+end Wasabee_common;
