8efc3bdb10d9c8a77c9efe292b4b7abbb1c9d3c9
gdemont <gdemont@d0ae7f95-e485-46ef-b4d0-40183c02ebb0>
2013-07-19 06:52:47 +0000

    Added a 'text' target and a beginning of an abstract rendering

--- trunk/common/wasabee_common.ads
+++ trunk/common/wasabee_common.ads
@@ -1,9 +1,14 @@
 with Ada.Finalization;
+with Ada.Strings.UTF_Encoding;
+with Ada.Strings.Wide_Unbounded;        use Ada.Strings.Wide_Unbounded;
 
 package Wasabee_common is
 
   type HTML_object is new Ada.Finalization.Controlled with null record;
 
   procedure Load_frame(object: in out HTML_object; URL: String);
+
+  subtype UTF_16_String is Ada.Strings.UTF_Encoding.UTF_16_Wide_String;
+  subtype UTF_16_Unbounded_String is Unbounded_Wide_String;
   
 end Wasabee_common;
