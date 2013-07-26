7656cfe3cc905cc09b01675e198ab740bb3a0d3c
gdemont <gdemont@d0ae7f95-e485-46ef-b4d0-40183c02ebb0>
2013-07-26 21:06:19 +0000

    (gwindows target) window and tab count done, proper app closing

--- target/gwindows/wasabee_gwin-main.ads
+++ target/gwindows/wasabee_gwin-main.ads
@@ -9,24 +9,24 @@ with GWindows.Windows;                  use GWindows.Windows;
 
 package Wasabee_GWin.Main is
 
-  type Window_access is access Wasabee_GWin.Windows.Wasa_Window_type;
+  type Window_access is access all Wasabee_GWin.Windows.Wasa_Window_type;
 
   package Windows_Vectors is new Ada.Containers.Vectors(
     Index_Type   => Positive,
     Element_Type => Window_access
   );
 
-  type Main_Window_Type is
+  type Main_Wasa_Window_Type is
     new GWindows.Windows.Main.Main_Window_Type with record
       windows       : Windows_Vectors.Vector;
       control_frame : Wasabee_Resource_GUI.Main_control_window_Type;
     end record;
 
   overriding
-  procedure On_Create (Window : in out Main_Window_Type);
+  procedure On_Create (Window : in out Main_Wasa_Window_Type);
 
-  procedure New_Wasa_Window(Window : in out Main_Window_Type);
+  procedure New_Wasa_Window(Window : in out Main_Wasa_Window_Type);
 
-  procedure Update_control_frame(Window : in out Main_Window_Type);
+  procedure Update_control_frame(Window : in out Main_Wasa_Window_Type);
 
 end Wasabee_GWin.Main;
--- target/gwindows/wasabee_gwin-windows.ads
+++ target/gwindows/wasabee_gwin-windows.ads
@@ -4,7 +4,7 @@ with Ada.Containers.Vectors;
 
 with GWindows;                          use GWindows;
 with GWindows.Windows;                  use GWindows.Windows;
--- with GWindows.Windows.Main;             use GWindows.Windows.Main;
+with GWindows.Windows.Main;             use GWindows.Windows.Main;
 
 package Wasabee_GWin.Windows is
 
@@ -18,10 +18,14 @@ package Wasabee_GWin.Windows is
   type Wasa_Window_Type is
     new GWindows.Windows.Window_Type with record
       tabs: Tabs_Vectors.Vector;
-      -- main: Pointer_To_Main_Window_Class;
+      main: Pointer_To_Main_Window_Class;
     end record;
 
   overriding
   procedure On_Create (Window : in out Wasa_Window_Type);
+
+  overriding
+  procedure On_Close (Window    : in out Wasa_Window_Type;
+                      Can_Close :    out Boolean);
 
 end Wasabee_GWin.Windows;
