e08f94df798c751b948f00c00396764bb267f1b8
gdemont <gdemont@d0ae7f95-e485-46ef-b4d0-40183c02ebb0>
2013-07-27 10:04:26 +0000

    Bichonnage sur récupération HTML

--- farm/main.adb
+++ farm/main.adb
@@ -19,15 +19,16 @@ procedure Main is
    Client  : Socket_Type;
    Address : Sock_Addr_Type;
    Channel : Stream_Access; 
-   CRLF : constant String := ASCII.CR & ASCII.LF;
-   Send   : String := (1 => ASCII.CR, 2 => ASCII.LF, 
-                       3 => ASCII.CR, 4 => ASCII.LF);
-   Data   : Ada.Streams.Stream_Element_Array (1 .. 256);
+   CRLF    : constant String := ASCII.CR & ASCII.LF;
+   Send    : constant String := CRLF & CRLF;
+   Max_Size: constant:= 256;
+   Data    : Ada.Streams.Stream_Element_Array (1 .. Max_Size);
    Size    : Ada.Streams.Stream_Element_Offset;
+   Data_Str: String(1 .. Max_Size);
    Ret     : Ada.Strings.Unbounded.Unbounded_String;
 begin
 
-   GNAT.Sockets.Initialize;  -- initialize a specific package
+   GNAT.Sockets.Initialize;  -- initialize a specific package - Obsolescent !!
    Create_Socket (Client);
    Address.Addr := Addresses (Get_Host_By_Name (Argument(1)), 1);
    
@@ -37,16 +38,17 @@ begin
    Channel := Stream (Client);
 
    String'Write (Channel, "GET " & Argument(2) &" HTTP/1.1" & CRLF &
-		   "Host: " & Argument(1) & CRLF & CRLF);
+                 "Host: " & Argument(1) & Send);
    
    loop 
       GNAT.Sockets.Receive_Socket(Client,Data,Size);
       Put_Line(Size'Img);
 
       for i in 1 .. Size loop
-	 Ret := Ret & Character'Val(Data(i));
+	 Data_Str(Integer(i)):= Character'Val(Data(i));
       end loop;
-      exit when Size < 256 ;
+      Ret:= Ret & Data_Str(1..Integer(Size));
+      exit when Size < Max_Size ;
    end loop;
    Ada.Text_IO.Put_Line (Ada.Strings.Unbounded.To_String(Ret));   
    -- Write it out to the console
