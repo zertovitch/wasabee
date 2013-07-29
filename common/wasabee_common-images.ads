cf0f421deeefc54105a1f3359a393f652eb4143f
gdemont <gdemont@d0ae7f95-e485-46ef-b4d0-40183c02ebb0>
2013-07-29 16:07:25 +0000

    Added wasabee_common-images with a 'with GID'

---
+++ common/wasabee_common-images.ads
@@ -0,0 +1,9 @@
+-- This is a draft and may change much,
+-- depending on how the bitmap abstraction is implemented.
+
+package Wasabee_common.Images is
+
+  procedure Decode(image_data: Unbounded_String);
+  -- Same for progressive decoding
+
+end Wasabee_common.Images;
