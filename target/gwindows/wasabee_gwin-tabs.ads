6fbf4a4f03cc175d5fade11bcb50021b136daff3
gdemont <gdemont@d0ae7f95-e485-46ef-b4d0-40183c02ebb0>
2013-07-30 13:34:30 +0000

    Added definitions for browser controls

--- target/gwindows/wasabee_gwin-tabs.ads
+++ target/gwindows/wasabee_gwin-tabs.ads
@@ -3,19 +3,20 @@ with Wasabee_common;                    use Wasabee_common;
 with GWindows.Cursors;
 with GWindows.Drawing;                  use GWindows.Drawing;
 with GWindows.Drawing_Objects;          use GWindows.Drawing_Objects;
-with GWindows.Drawing_Panels;
+with GWindows.Drawing_Panels;           use GWindows.Drawing_Panels;
 with GWindows.Types;
 
 package Wasabee_GWin.Tabs is
 
-  type Tab_type is new GWindows.Drawing_Panels.Drawing_Panel_Type with record
+  type Tab_type is new Drawing_Panel_Type with record
     HTML_contents: Wasabee_common.HTML_object;
     --
-    --  Windows graphics, inckuding double-buffering
+    --  Windows graphics, including double-buffering
     --
     Drawing_Area : Canvas_Type;
     Saved_Area   : Memory_Canvas_Type;
     Saved_Bitmap : Bitmap_Type;
+    --
     Cursor       : GWindows.Cursors.Cursor_Type;
   end record;
 
--- target/gwindows/wasabee_gwin-windows.ads
+++ target/gwindows/wasabee_gwin-windows.ads
@@ -1,11 +1,15 @@
 with Wasabee_GWin.Tabs;
-
-with Ada.Containers.Vectors;
+with Wasabee_Resource_GUI;              use Wasabee_Resource_GUI;
 
 with GWindows;                          use GWindows;
+with GWindows.Edit_Boxes;               use GWindows.Edit_Boxes;
+with GWindows.Drawing_Panels;           use GWindows.Drawing_Panels;
+with GWindows.Packing_Boxes;            use GWindows.Packing_Boxes;
+with GWindows.Panels;                   use GWindows.Panels;
 with GWindows.Windows;                  use GWindows.Windows;
 with GWindows.Windows.Main;             use GWindows.Windows.Main;
-with Wasabee_Resource_GUI;              use Wasabee_Resource_GUI;
+
+with Ada.Containers.Vectors;
 
 package Wasabee_GWin.Windows is
 
@@ -16,19 +20,26 @@ package Wasabee_GWin.Windows is
     Element_Type => Tab_access
   );
 
-  type Browser_window_type is
-    new GWindows.Windows.Window_Type with record
-      tabs: Tabs_Vectors.Vector;
-      main: Pointer_To_Main_Window_Class;
-      hidden_menu: Browser_Menu_Type;
-    end record;
+  type Control_Box_Type is new Packing_Box_Type with record
+    tab_carets         : Drawing_Panel_Type;
+    navigarion_and_url : Packing_Box_Type;
+    navigation_buttons : Panel_Type;
+    url_box            : Edit_Box_Type;
+  end record;
+
+  type Browser_window_type is new GWindows.Windows.Window_Type with record
+    tabs        : Tabs_Vectors.Vector;
+    control_box : Control_Box_Type;
+    main        : Pointer_To_Main_Window_Class;
+    hidden_menu : Browser_Menu_Type;
+  end record;
 
   overriding
   procedure On_Create (Window : in out Browser_window_type);
 
   procedure New_Tab(Window : in out Browser_window_type);
 
-  -- NB: the menu is invisible; only commands and shortcuts used
+  -- NB: the menu is not created; only commands and shortcuts are used
 
   overriding
   procedure On_Menu_Select (
