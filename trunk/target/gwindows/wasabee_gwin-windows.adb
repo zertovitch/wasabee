2751d11a3bdbe88cf56b23910ec8811b43572b70
gdemont <gdemont@d0ae7f95-e485-46ef-b4d0-40183c02ebb0>
2013-07-30 22:45:51 +0000

    (gwindows) some progress on browser controls

--- trunk/target/gwindows/wasabee_gwin-windows.adb
+++ trunk/target/gwindows/wasabee_gwin-windows.adb
@@ -1,11 +1,60 @@
 with Wasabee_GWin.Main;                 use Wasabee_GWin.Main;
 with Wasabee_GWin.Tabs;                 use Wasabee_GWin.Tabs;
 
+with GWindows.Base;                     use GWindows.Base;
+
 package body Wasabee_GWin.Windows is
+
+  overriding
+  procedure On_Create (Window : in out Control_Box_Type) is
+  begin
+    Window.navigation_and_url.Create_As_Control(
+      Parent     => Window,
+      Left       => 0,
+      Top        => 20,
+      Width      => 1, -- will be extended by docking
+      Height     => 1  -- will be extended by docking
+    );
+    Window.navigation_buttons.Create_As_Control(
+      Parent     => Window.navigation_and_url,
+      Left       => 0,
+      Top        => 0,
+      Width      => 50,
+      Height     => 1  -- will be extended by docking
+    );
+    Window.navigation_buttons.Dock(At_Left);
+    Window.url_box.Create(
+      Parent     => Window.navigation_and_url,
+      Left       => 50,
+      Top        => 0,
+      Width      => 1, -- will be extended by docking
+      Height     => 1, -- will be extended by docking
+      Text       => "Click here or press Ctrl-L for entering new Web address"
+    );
+    Window.url_box.Dock(Fill);
+    Window.tab_visuals.Create_As_Control(
+      Parent     => Window,
+      Left       => 0,
+      Top        => 0,
+      Width      => 1,  -- will be extended by docking
+      Height     => 20
+    );
+    Window.tab_visuals.Dock(At_Top);
+    Window.navigation_and_url.Dock(Fill);
+  end On_Create;
 
   overriding
   procedure On_Create (Window : in out Browser_window_type) is
   begin
+    Use_Gui_Font(Window);
+    Window.control_box.Create_As_Control(
+      Parent     => Window,
+      Left       => 0,
+      Top        => 0,
+      Width      => 1, -- will be extended by docking
+      Height     => 40
+    );
+    Window.control_box.Dock(At_Top);
     --  Wasabee_Resource_GUI.Create_Full_Menu(Window.hidden_menu);
     --  Window.Menu(Window.hidden_menu.Main);
     Accelerator_Table (Window, "Browser_Window_Shortcuts");
@@ -21,14 +70,13 @@ package body Wasabee_GWin.Windows is
       renames Main_Wasa_Window_Type(Window.main.all);
   begin
     newcomer.Create_As_Control(
-      Window,
-      "Another Tab",
-      0,
-      0,
-      Window.Client_Area_Width,
-      Window.Client_Area_Height
+      Parent => Window,
+      Left   => 0,
+      Top    => 20,
+      Width  => Window.Client_Area_Width,
+      Height => Window.Client_Area_Height
     );
-    newcomer.Show;
+    newcomer.Dock(Fill);
     Window.tabs.Append(newcomer);
     main_window.Update_control_frame;
   end New_Tab;
