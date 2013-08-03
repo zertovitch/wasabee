792b1b9994c901be7a950bc0b97fdf18b425554c
ciceronne <ciceronne@d0ae7f95-e485-46ef-b4d0-40183c02ebb0>
2013-08-03 04:49:37 +0000

    Adding URL

---
+++ common/wasabee-url.ads
@@ -0,0 +1,20 @@
+with Ada.Strings.Unbounded ; use Ada.Strings.Unbounded ;
+
+package Wasabee.URL is
+
+   type URL is record
+      Protocole  : Unbounded_String ; -- http generalement
+      Host       : Unbounded_String ;
+      Port       : Natural ;  -- 80 generalement
+      Ressource  : Unbounded_String ;
+   end record;
+
+   procedure To_String (U : in URL) ;
+
+   procedure Decode (Adr : in Unbounded_String ; U : in out URL) ;
+
+   procedure Test (U : String) ;
+
+   procedure General_Test ;
+
+end ;
