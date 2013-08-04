9eb429fe8f401cd7e4b892c4e78009a4e5a02de1
gdemont <gdemont@d0ae7f95-e485-46ef-b4d0-40183c02ebb0>
2013-08-04 20:51:23 +0000

    (GWindows) HTML_area now correctly with a Scroll_Panel

--- trunk/target/gwindows/wasabee-gwin-display.adb
+++ trunk/target/gwindows/wasabee-gwin-display.adb
@@ -1,55 +1,30 @@
-with GWindows.Application;
+-- with GWindows.Application;
+-- with GWindows.Message_Boxes;            use GWindows.Message_Boxes;
 with GWindows.Types;                    use GWindows.Types;
 -- with GWindows.Colors;                   use GWindows.Colors;
 -- with GWindows.Message_Boxes;            use GWindows.Message_Boxes;
 
 package body Wasabee.GWin.Display is
 
+  procedure Clear_area (on: in out Wasa_GWin_Panel) is
+  begin
+    Fill_Rectangle (
+      Canvas      => on.Drawing_Canvas,
+      Rectangle   => (0, 0, on.Draw_Control.Width, on.Draw_Control.Height),
+      Color_Const => -1
+      -- Integer(GWindows.Colors.White) 
+      -- O_o: GWindows bug ? Integer (signed) vs Color_Type (unsigned!)
+    );
+  end Clear_area;
+  
   procedure Text_XY (
-    on   : in out Wasa_GWin_Canvas; 
+    on   : in out Wasa_GWin_Panel; 
     x,y  : in     Integer; 
     text : in     UTF_16_String
   )
   is
   begin
-    on.mem_windows_canvas.Put(x,y,text);
+    on.Drawing_Canvas.Put(x,y,text);
   end Text_XY;
-
-  procedure Draw_object(
-    Canvas : in out Wasa_GWin_Canvas;
-    ho     : in out HTML_Object; -- in out: ho.refresh is changed
-    width  : Integer;
-    height : Integer)
-  is
-    -- Max_Viewport: constant Size_Type:= Viewport_Extents(Canvas);
-    Max_Rectangle: constant Rectangle_Type:=
-      (0, 0, 
-       GWindows.Application.Desktop_Width, -- !! Max_Viewport.Width, 
-       GWindows.Application.Desktop_Height -- !! Max_Viewport.Height
-      );
-
-    procedure Clear_Area is
-    begin
-      Fill_Rectangle (
-        Canvas => Canvas.mem_windows_canvas,
-        Rectangle => Max_Rectangle,
-        Color_Const => -1
-        -- Integer(GWindows.Colors.White) -- O_o: Integer vs Color_Type (unsigned!)
-      );
-    end Clear_Area;
-
-  begin
-    -- Here we call the overriden Draw with display methods for GWindows 
-    Clear_Area; -- !! should be as abstract, called by Draw
-    Canvas.Draw(ho);
-    Canvas.Text_XY(
-        10, 40,
-        "Dro! Dro!" & 
-        Integer'Wide_Image(Max_Rectangle.Right) & 
-        Integer'Wide_Image(Max_Rectangle.Bottom) &
-        Integer'Wide_Image(width) & 
-        Integer'Wide_Image(height)
-        );
-  end Draw_object;
 
 end Wasabee.GWin.Display;
