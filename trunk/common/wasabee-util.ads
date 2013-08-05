ee75ac0f64636c5aaa536b18c6fafaa8bfb38cf7
gdemont <gdemont@d0ae7f95-e485-46ef-b4d0-40183c02ebb0>
2013-08-05 17:08:49 +0000

    <title> correctly retrieved :-)

--- trunk/common/wasabee-util.ads
+++ trunk/common/wasabee-util.ads
@@ -1,8 +1,11 @@
 with Ada.Strings.Unbounded;             use Ada.Strings.Unbounded;
+with Ada.Strings.Wide_Unbounded;        use Ada.Strings.Wide_Unbounded;
 
 package Wasabee.Util is
 
   function S (Source : Unbounded_String) return String renames To_String;
+  function S (Source : Unbounded_Wide_String) return Wide_String renames To_Wide_String;
   function U (Source : String) return Unbounded_String renames To_Unbounded_String;
+  function U (Source : Wide_String) return Unbounded_Wide_String renames To_Unbounded_Wide_String;
 
 end Wasabee.Util;
