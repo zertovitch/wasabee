80039dc092a907596a57da28804df54a97f28d7f
ciceronne <ciceronne@d0ae7f95-e485-46ef-b4d0-40183c02ebb0>
2013-08-11 17:58:47 +0000

    Adding some samples

--- target/gwindows/build.cmd
+++ target/gwindows/build.cmd
@@ -4,10 +4,6 @@ rem e.g. "-march=i686" for MinGW 4.7.2
 REM Example for the variable ADA_INCLUDE_PATH
 REM C:\Ada\zip-ada\zip_lib;C:\Ada\gnavi\gwindows\framework;C:\Ada\gnavi\gwindows\contrib;C:\Ada\gnavi\gnatcom\framework;C:\GNAT\2013\include\xmlada
 
-set ADA_INCLUDE_PATH=C:\Projects\gnavi-code\gwindows\framework;%ADA_INCLUDE_PATH%
-set ADA_INCLUDE_PATH=C:\Projects\gnavi-code\gwindows\contrib;%ADA_INCLUDE_PATH%
-set ADA_INCLUDE_PATH=C:\Projects\gnavi-code\gnatcom\framework;%ADA_INCLUDE_PATH%
-set ADA_INCLUDE_PATH=C:\GNAT\2013\include\xmlada;%ADA_INCLUDE_PATH%
 
 rem *** Build in Fast mode
 
