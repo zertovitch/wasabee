41b7147c78452b8d473e43fd8aeed36bc070f2ed
ciceronne <ciceronne@d0ae7f95-e485-46ef-b4d0-40183c02ebb0>
2013-08-12 23:17:46 +0000

    Sage comme une image

--- trunk/common/wasabee_image.adb
+++ trunk/common/wasabee_image.adb
@@ -2,6 +2,7 @@
 --
 --
 
+
 with Ada.Text_IO                  ; use Ada.Text_IO ;
 with Ada.Strings.Unbounded        ; use Ada.Strings.Unbounded;
 
@@ -14,58 +15,16 @@ with Wasabee.Request   ;      use Wasabee.Request ;
 with Wasabee.Xhtml ;          use Wasabee.Xhtml ;
 with Wasabee.Url   ;          use Wasabee.Url ;
 with Wasabee.Net   ;          use Wasabee.Net ;
+with Wasabee.Images ;         use Wasabee.Images ;
 
 with GNAT.Sockets ; use GNAT.Sockets ;
 with Interfaces; use Interfaces ;
 with Ada.Unchecked_Deallocation;
 with Ada.Calendar;
-
+with Ada.Streams                      ; use Ada.Streams                      ;
 with GID ;
 
 procedure Wasabee_Image is
-   
-   
-   type Byte_Array is array(Integer range <>) of Unsigned_8;
-   type p_Byte_Array is access Byte_Array;
-   procedure Dispose is new Ada.Unchecked_Deallocation(Byte_Array, p_Byte_Array);
-   
-   --
-   -- But du jeu, a partir d'un node, obtenir un Handler GID sur une image
-   -- Easy non ?
-   --
-   procedure Get_Image (Nd : in Node ; Img : in out GID.Image_Descriptor) is
-      A            : Attr ;
-      The_Url      : Wasabee.Url.URL ;      
-      Client       : Socket_Type ;
-      Address      : Sock_Addr_Type;
-      Channel      : Stream_Access ;
-      Send         : constant String :=  (1 => ASCII.CR, 2 => ASCII.LF,
-                                          3 => ASCII.CR, 4 => ASCII.LF);  
-      next_frame, current_frame: Ada.Calendar.Day_Duration:= 0.0;
-      
-   begin
-      A := Get_Named_Item(Attributes(Nd),"src");
-      Put_Line(Value(A)) ;
-      -- Ok essayons de l'ouvrir en HTTP maintenant ...
-      Decode(To_Unbounded_String(Value(A)), The_Url);
-      Display_URL_Details(The_Url);      
-      
-      GNAT.Sockets.Initialize;
-      Create_Socket(Client);
-      Put_Line("Trying to get IP: " & To_String(The_Url.Host));
-      Address.Addr := To_IP_Address(To_String(The_Url.Host));
-      Address.Port := The_URL.Port;
-      Put_Line("Trying to open " & 
-		 To_String(The_Url.Host) & 
-		 ", Port " & 
-		 Port_Type'Image(The_Url.Port));
-      Connect_Socket (Client, Address);
-      Channel := Stream(Client);
-      GID.Load_image_header(Img,
-			    Channel.all);
-      
-   end ;
-   
    Xhtml, Nl : Node_List ;
    Xhtml_Content : Unbounded_String ;
    N : Node;
@@ -88,4 +47,9 @@ begin
    
    -- Ok maintenant j'ai le bon attribut
    Get_Image(N, Img);
+   -- Getting width
+   Put_Line("Width: " & Positive'Image(GID.Pixel_Width(Img)));
+   -- Getting height
+   Put_Line("Height: " & Positive'Image(GID.Pixel_height(Img)));
+   
 end ;
