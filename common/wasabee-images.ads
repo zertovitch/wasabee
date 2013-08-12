709b08cfb755f8e6327f4497e2c56512a5c5ab9c
ciceronne <ciceronne@d0ae7f95-e485-46ef-b4d0-40183c02ebb0>
2013-08-12 23:16:42 +0000

    Sage comme une image

--- common/wasabee-images.ads
+++ common/wasabee-images.ads
@@ -3,9 +3,37 @@
 
 with Ada.Strings.Unbounded;             use Ada.Strings.Unbounded;
 
+
+with Ada.Text_IO                  ; use Ada.Text_IO ;
+with Ada.Strings.Unbounded        ; use Ada.Strings.Unbounded;
+
+with Dom.Core                     ; use Dom.Core ;
+with Dom.Core.Elements            ; use Dom.Core.Elements ;
+with Dom.Core.Nodes               ; use Dom.Core.Nodes ; 
+with Dom.Core.Attrs               ; use Dom.Core.Attrs ;
+
+with Wasabee.Request   ;      use Wasabee.Request ;
+with Wasabee.Xhtml ;          use Wasabee.Xhtml ;
+with Wasabee.Url   ;          use Wasabee.Url ;
+with Wasabee.Net   ;          use Wasabee.Net ;
+
+with GNAT.Sockets ; use GNAT.Sockets ;
+with Interfaces; use Interfaces ;
+with Ada.Unchecked_Deallocation;
+with Ada.Calendar;
+with Ada.Streams                      ; use Ada.Streams                      ;
+with GID ;
+
 package Wasabee.Images is
 
   procedure Decode(image_data: Unbounded_String);
   -- Same for progressive decoding
-
+  
+  
+  function Read_Line (Channel : in Stream_Access) return String ;
+  
+  procedure Get_Image (Nd : in Node ; Img : in out GID.Image_Descriptor) ;
+  
+  
+  
 end Wasabee.Images;
