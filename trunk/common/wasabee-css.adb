f448bdf0659c217179bbd1341515a9f5d055ae0a
gdemont <gdemont@d0ae7f95-e485-46ef-b4d0-40183c02ebb0>
2013-08-26 19:00:10 +0000

    W.CSS update

--- trunk/common/wasabee-css.adb
+++ trunk/common/wasabee-css.adb
@@ -25,26 +25,22 @@ package body Wasabee.Css is
       Tmp : Unbounded_String ;
       C : Character ;
    begin
-
       for I in 1..Length(Content) loop
          C := Element(Content,I) ;
          -- On ne prend pas les espaces
          -- et les tabulations
-
          if Character'Pos(C) = 9 then
             null ;
          elsif Character'Pos(C) = 10 then
             null ;
          else
-            -- Put_Line(C & ":" & Integer'Image(Character'Pos(C))) ;
             Append(Tmp, C);
          end if;
-
       end loop;
       return Tmp ;
    end;
 
-   procedure Read_CSS_File (S: String) is
+   procedure Read_CSS_File (S: String ; Css : in out Unbounded_String) is
       Filename : String := S ;
       File : File_Type ;
       Line_Count : Natural := 0 ;
@@ -52,13 +48,15 @@ package body Wasabee.Css is
       Ada.Text_IO.Open(File, In_File, Filename);
       while not End_Of_File (File) loop
          Line_Count := Line_Count + 1 ;
-         -- Put_Line (Natural'Image(Line_Count) & ": " & Line) ;
+         -- Ada.Text_IO.Put_Line (Get_Line(File));
          Append(Css , Get_Line(File));
       end loop;
       Close(File);
    end;
 
-   procedure Parse_CSS_Element (TKey : String ; Element : Unbounded_String) is
+   procedure Parse_CSS_Element (Map : in out CSS_Dictionary.Map ;
+                                TKey : String ;
+                                Element : Unbounded_String) is
       Key : Unbounded_String ;
       Value : Unbounded_String ;
       Count : Natural := 0;
@@ -71,21 +69,22 @@ package body Wasabee.Css is
          Aff_Pos := Index (Element, ":" , Base);
          exit when Aff_Pos = 0 ;
          Sep_Pos := Index (Element, ";" , Aff_Pos+1);
-	 
-	 if Sep_Pos = 0 then
-	    Sep_Pos := Length(Element);
-	 end if;
-	 
+
+         if Sep_Pos = 0 then
+            Sep_Pos := Length(Element);
+         end if;
+
          Key := Unbounded_Slice(Element, Base, Aff_Pos - 1) ;
          Value := Unbounded_Slice(Element, Aff_Pos + 1, Sep_Pos - 1) ;
          Put_Line("Key:" & To_String(Key));
          Put_Line("Value:" & To_String(Value));
-	 Add_Or_Replace_CSS(TKey, To_String(Key) , To_String(Value)) ;	 
-	 Base := Sep_Pos + 1 ;
+         Add_Or_Replace_CSS(Map , TKey, To_String(Key) , To_String(Value)) ;
+         Base := Sep_Pos + 1 ;
       end loop ;
    end ;
 
-   procedure Parse_Information is
+   procedure Parse_Information (Map : in out CSS_Dictionary.Map ;
+                                Css : Unbounded_String) is
       Base : Natural := 1;
       Openbracket, Closebracket : Natural ;
       Key : Unbounded_String ;
@@ -103,99 +102,104 @@ package body Wasabee.Css is
          --Put_Line("Ouverture : " & Integer'Image(Base)) ;
          --Put_Line("Fermeture : " & Integer'Image(Closebracket)) ;
          Css_Content := Unbounded_Slice(Css, Openbracket+1, Closebracket-1);
-	 Put_Line("CSS Content:" & To_String(Css_Content));
+         Put_Line("CSS Content:" & To_String(Css_Content));
          Css_Content := Clean(Css_Content);
-         Parse_Css_Element (To_String(Key), Css_Content);
+         Parse_Css_Element (Map , To_String(Key), Css_Content);
          New_Line ;
          Base := Closebracket+1 ;
       end loop;
    end;
 
-   procedure Set_CSS_Value ( Content : String ) is
-   begin
-      CSS := To_Unbounded_String(Content) ;
-   end;
-
-   procedure Get_Css_Unit_Element (TKey : String ; Content : String) is
+   procedure Get_Css_Unit_Element (Map : in out CSS_Dictionary.Map ;
+                                   TKey : String ;
+                                   Content : String ;
+                                   Css : Unbounded_String) is
       Base : Natural := 1;
       -- Openbracket,
       Closebracket : Natural ;
       Key : Unbounded_String := To_Unbounded_String(Content) ;
    begin
-      Closebracket := Index(Css, "}", Base+1) ;
+      Closebracket := Index(Css , "}", Base+1) ;
       Key := Clean_Key(Key);
       -- Put_Line("CSS Element:" & To_String(Key));
       -- Put_Line("Ouverture : " & Integer'Image(Base)) ;
       -- Put_Line("Fermeture : " & Integer'Image(Closebracket)) ;
       -- Put_Line("CSS Content:" & To_String(Key));
-      Parse_Css_Element (TKey, key);
+      Parse_Css_Element (map, TKey, key);
       New_Line ;
    end ;
-   
+
    --
    -- Les deux fonctions phares de Wasabee pour le CSS
    --
-   
-   procedure Add_Or_Replace_CSS (Element : String ; Key : String ; Value : String) is
+
+   procedure Add_Or_Replace_CSS (Map : in out CSS_Dictionary.Map ;
+                                 Element : String ;
+                                 Key : String ;
+                                 Value : String) is
       Ptr : CSS_Properties_Map_Ptr ;
+      Cleaned_Key : Unbounded_String ;
    begin
-      
+
+      Cleaned_Key := Clean_Key(To_Unbounded_String(Key)) ;
+
       Put_Line("[ADD_OR_REPLACE_CSS] " & Element & " : " & Key & " => " & Value);
-      
       --
       -- Je cherche si il existe un tableau de propriétés pour cet élément
-      --      
-      if CSS_Dictionary.Contains(Css_Page_Dictionary, To_Unbounded_String(Element)) then
-	 Put_Line("[ADD_OR_REPLACE_CSS] Properties exists ..");
-	 Ptr := CSS_Dictionary.Element(Css_Page_Dictionary, To_Unbounded_String(Element)) ;
-      else	 
-	 Put_Line("[ADD_OR_REPLACE_CSS] Properties to create ..");
-	 Ptr := new CSS_Properties.Map;
-	 CSS_Dictionary.Insert(Css_Page_Dictionary, To_Unbounded_String(Element), Ptr);
+      --
+      if CSS_Dictionary.Contains(Map, To_Unbounded_String(Element)) then
+         Put_Line("[ADD_OR_REPLACE_CSS] Properties exists ..");
+         Ptr := CSS_Dictionary.Element(Map, To_Unbounded_String(Element)) ;
+      else
+         Put_Line("[ADD_OR_REPLACE_CSS] Properties to create ..");
+         Ptr := new CSS_Properties.Map;
+         CSS_Dictionary.Insert(Map, To_Unbounded_String(Element), Ptr);
       end if ;
-      
+
       --
       -- Ok on a un container
       --
-      
+
       Put_Line("[ADD_OR_REPLACE_CSS] Inserting Element") ;
       CSS_Properties.Insert( Ptr.all,
-			     To_Unbounded_String(Key),
-			     To_Unbounded_String(Value) ) ;
-      
+                             To_Unbounded_String(Key),
+                             To_Unbounded_String(Value) ) ;
+
    exception
       when others =>
-	 null ;
+         null ;
    end ;
-   
-   function  Get_CSS (Element : String ; Key : String) return String is
+
+   function  Get_CSS (Map : CSS_Dictionary.Map ;
+                      Element : String ;
+                      Key : String) return String is
       Ptr : CSS_Properties_Map_Ptr ;
    begin
-      
+
       Put_Line("[GETTING CSS] " & Element & " with key " & Key);
-      
+
       --
       -- Je cherche si il existe un tableau de propriétés pour cet élément
       --
-      if CSS_Dictionary.Contains(Css_Page_Dictionary, To_Unbounded_String(Element)) then
-	 Put_Line("[GETTING CSS] Properties exists");
-	 Ptr := CSS_Dictionary.Element(Css_Page_Dictionary, To_Unbounded_String(Element)) ;
-      else	 
-	 Put_Line("[GETTING CSS] Properties does not exists");
-	 return "" ;
-      end if ;
-      
-      if CSS_Properties.Contains(Ptr.all , To_Unbounded_String(Key)) then
-	 Put_Line("[GETTING CSS] Property exist ");
-	 return To_String(CSS_Properties.Element(Ptr.all, To_Unbounded_String(Key))) ;
+      if CSS_Dictionary.Contains(Map, To_Unbounded_String(Element)) then
+         Put_Line("[GETTING CSS] Properties exists");
+         Ptr := CSS_Dictionary.Element(Map, To_Unbounded_String(Element)) ;
       else
-	 Put_Line("[GETTING CSS] Property doesnot exist ");
-	 Put_Line("[GETTING CSS] ");
-	 return "" ;
-      end if ;      
-      
+         Put_Line("[GETTING CSS] Properties does not exists");
+         return "" ;
+      end if ;
+
+      if CSS_Properties.Contains(Ptr.all , To_Unbounded_String(Key)) then
+         Put_Line("[GETTING CSS] Property exist ");
+         return To_String(CSS_Properties.Element(Ptr.all, To_Unbounded_String(Key))) ;
+      else
+         Put_Line("[GETTING CSS] Property doesnot exist ");
+         Put_Line("[GETTING CSS] ");
+         return "" ;
+      end if ;
+
    end ;
-   
-   
-   
+
+
+
 end ;
--- trunk/common/wasabee-css.ads
+++ trunk/common/wasabee-css.ads
@@ -3,55 +3,57 @@ with Ada.Command_Line ; use Ada.Command_Line ;
 with Ada.Strings.Unbounded ; use Ada.Strings.Unbounded ;
 
 with Ada.Containers.Vectors ;
-with Ada.Strings.Unbounded.Hash ; 
+with Ada.Strings.Unbounded.Hash ;
 with Ada.Containers.Hashed_Maps ;
 
 package Wasabee.Css is
-   
+
    --
    -- Definissions quelques types bien utiles
    --
-   
+
    package CSS_Properties is new Ada.Containers.Hashed_Maps (Unbounded_String,
-							     Unbounded_String,
-							     Hash,
-							     "=");
-   
+                                                             Unbounded_String,
+                                                             Hash,
+                                                             "=");
+
    use CSS_Properties ;
-   
+
    type CSS_Properties_Map_Ptr is access all CSS_Properties.Map ;
-   
-   
+
+
    package CSS_Dictionary is new Ada.Containers.Hashed_Maps (Unbounded_String,
-							     CSS_Properties_Map_Ptr,
-							     Hash, "=") ;
-   
-   Css_Page_Dictionary : CSS_Dictionary.Map ;
-   
-   procedure Add_Or_Replace_CSS (Element : String ; Key : String ; Value : String) ;
-   
-   function  Get_CSS (Element : String ; Key : String) return String ;
-   
+                                                             CSS_Properties_Map_Ptr,
+                                                             Hash, "=") ;
+
+   procedure Add_Or_Replace_CSS (Map : in out CSS_Dictionary.Map ;
+                                 Element : String ;
+                                 Key : String ; Value : String) ;
+
+   function  Get_CSS (Map : CSS_Dictionary.Map ;
+                      Element : String ;
+                      Key : String) return String ;
+
    --
-   Css : Unbounded_String ;
+   -- Css : Unbounded_String ;
    --
 
    -- Les properties generale de la page
    General_Properties : access CSS_Properties.Map ;
-   
+
    --
    -- Vecteur de propriétés
    --
 
    function  Clean (Content : in Unbounded_String) return Unbounded_String ;
 
-   procedure Read_CSS_File (S: String) ;
+   procedure Read_CSS_File (S: String ; Css : in out Unbounded_String) ;
 
-   procedure Parse_CSS_Element (TKey : String ; Element : Unbounded_String) ;
+   procedure Parse_CSS_Element (Map : in out CSS_Dictionary.Map ;
+                                TKey : String ;
+                                Element : Unbounded_String) ;
 
-   procedure Parse_Information ;
-
-   procedure Set_CSS_Value ( Content : String ) ;
+   procedure Parse_Information (Map : in out CSS_Dictionary.Map ; Css : Unbounded_String) ;
 
 end ;
 
